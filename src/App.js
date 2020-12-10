b5f2208567b31625371e76840de389a4a461f2f5
e-compfast <alamhafidz01@gmail.com>
2020-12-11 00:00:19 +0700

    Membuat Struktur App Baru

--- src/App.js
+++ src/App.js
@@ -1,75 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-export default class App extends Component {
 
-  state = {
-    count: this.props.value,
-    tags: ['tags1', 'tags2', 'tags3']
-  };
-
-  
-
-  GetClass(){
-    let classes = 'badge m-3 badge-';
-        classes += this.state.count === 0 ? 'warning' : 'primary';
-      return classes;
-  }
-
-  handleTambah() {
-    console.log('click tambah', this)
-    const count = this.state.count;
-    this.setState({
-      count : count +1
-    })
-  }
-
-  GetTags(){
-    let mapTags = this.state.tags.map((tag, k)=>{
-      return (<li key={k}>{tag}</li>)
-    })
-
-    let noTags = this.state.tags.length === 0 ? (<p>They are no Tags</p>): (mapTags) ;
-    
-
-    return noTags;
-  }
-
-
-  
-
-
+export class App extends Component {
   render() {
     return (
-      <>
-
-      <div className='container'>
-        {
-          this.props.children 
-        }
-        @
-        {
-          this.props.countId
-        }
-        <span className={this.GetClass()}>{this.state.count === 0 ? 'zero' : this.state.count}</span>
-        <button onClick={()=>this.handleTambah()}className='btn btn-primary'>Tambah</button>
-        
-          <ul>{this.GetTags()}</ul>
-        
-      </div>
-      </>
+      <div></div>
     );
   }
-
-  componentDidMount() {
-  //   fetch('http://localhost:3088/posts')
-  //   .then(res=>{
-  //     return res.json()
-  //   })
-  //   .then(myjson=>{
-  //     console.log(myjson)
-  //   })
-  // }
-
-  axios.get('http://localhost:3088/posts').then(res=> console.log(res.data))
-}
 }
