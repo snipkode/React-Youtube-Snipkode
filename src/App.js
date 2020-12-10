bad827c233469ba68e566f09b3c3336b1b069720
e-compfast <alamhafidz01@gmail.com>
2020-12-10 16:23:40 +0700

    Membuat Design Vote System

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+  state = {
+    count: this.props.value,
+    tags: ['tags1', 'tags2', 'tags3']
+  };
+
+  GetClass(){
+    let classes = 'badge m-3 badge-';
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+      return classes;
+  }
+
+  handleTambah() {
+    console.log('click tambah', this)
+    const count = this.state.count;
+    this.setState({
+      count : count +1
+    })
+  }
+
+  GetTags(){
+    let mapTags = this.state.tags.map((tag, k)=>{
+      return (<li key={k}>{tag}</li>)
+    })
+
+    let noTags = this.state.tags.length === 0 ? (<p>They are no Tags</p>): (mapTags) ;
+    
+
+    return noTags;
+  }
+
   render() {
     return (
-      <div>
-        Hello Youtube Channel
+      <>
+
+      <div className='container'>
+        {
+          this.props.children 
+        }
+        @
+        {
+          this.props.countId
+        }
+        <span className={this.GetClass()}>{this.state.count === 0 ? 'zero' : this.state.count}</span>
+        <button onClick={()=>this.handleTambah()}className='btn btn-primary'>Tambah</button>
+        
+          <ul>{this.GetTags()}</ul>
+        
       </div>
+      </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Counter from './counter.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 ReactDOM.render(
 <Router>
 	<Switch>
-		<Route exact path={'/'} component={App}/>
+		<Route exact path={'/'} component={Counter}/>
 		<Route path={'*'} render={()=><>Nothing</>}/>
 	</Switch>
 </Router>, document.getElementById('root'));
