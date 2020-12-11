1b65e618bfdaf31ea1958d8d33f28847e836916a
snipkode <alamhafidz61@gmail.com>
2020-12-12 06:47:04 +0700

    Testing First Editing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          selamat datang
         </a>
       </header>
     </div>
